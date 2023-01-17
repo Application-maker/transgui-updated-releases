3e70d956b19e70c495ee59f34c9f36c1254964b5
Max-Gouliaev <68902211+Max-Gouliaev@users.noreply.github.com>
2023-01-17 23:28:43 +0300

    Shortened code + fixed issue that Released only the first file in Release directory

--- build-transgui.sh
+++ build-transgui.sh
@@ -1,4 +1,3 @@
-counter=0
 now=$(date +%F)
 
 if [ -d "./transgui" ]; then
@@ -13,12 +12,4 @@ cd ../../
 
 VERSION=$(cat VERSION.txt)
 
-filenames=("$( ls ./Release )")
-
-for i in "${filenames[@]}" ; do
-  filepaths[counter]="./Release/$i"
-  counter=$((counter+1))
-  echo "$i"
-done
-
-gh release create "$VERSION" -t "$now" --repo Max-Gouliaev/transgui-updated-releases -n "" "$filepaths"
+gh release create "$VERSION" -t "$now" --repo Max-Gouliaev/transgui-updated-releases -n "" ./Release/*
