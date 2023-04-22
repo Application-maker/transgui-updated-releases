f0b163abe7781d11f28f887261a23ec1fb11e03e
Max-Gouliaev <68902211+Max-Gouliaev@users.noreply.github.com>
2023-04-22 16:06:05 +0300

    Comment the code

--- build-transgui.sh
+++ build-transgui.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 now=$(date +%F)
 
+# Update the reposirory
 if [ -d "./transgui" ]; then
   rm -rf ./transgui
 fi
 
+# Clone the repository
 git clone https://github.com/transmission-remote-gui/transgui.git
+
+# Build the application
 cd ./transgui/setup/unix || exit
 ./build.sh
 
 cd ../../
 
+# Aknowledge version number
 VERSION=$(cat VERSION.txt)
 
+# Release the build
 gh release create "$VERSION" -t "$now" --repo Max-Gouliaev/transgui-updated-releases -n "" ./Release/*
