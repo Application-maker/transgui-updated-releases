c3859ec0acce2d044523ac4bc04711949b4c9e8c
Max-Gouliaev <68902211+Max-Gouliaev@users.noreply.github.com>
2023-05-12 19:18:45 +0000

    Install Chocolatey and git before using them (Windows)

--- build-transgui.bat
+++ build-transgui.bat
@@ -1,22 +1,6 @@
 @echo off
 set now=%date:~6,4%%date:~3,2%%date:~0,2%
 
-:: Check the arguments
-if NOT %1%==--no-clone (
-   if NOT %1%==-nc (
-      if NOT %1%==--no-update (
-         if NOT %1%==-nu (
-            if exist .\transgui\ (
-               :: Remove old repository files
-               rm -rf .\transgui
-               )
-            :: Clone the repository
-            git clone https://github.com/transmission-remote-gui/transgui.git
-            )
-         )
-      )
-   )
-
 :: BatchGotAdmin
 
    ::  --> Check for permissions
@@ -49,6 +33,37 @@ if NOT %1%==--no-clone (
    :gotAdmin
       pushd "%CD%"
       CD /D "%~dp0"
+
+
+where choco >nul 2>&1
+:: Install Chocolatey
+if %errorLevel% == 0 (
+   echo Found Chocolatey
+   choco --version
+) else (
+   echo Install Chocolatey...
+   @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+)
+
+:: Install git
+choco install git
+
+
+:: Check the arguments
+if NOT %1%==--no-clone (
+   if NOT %1%==-nc (
+      if NOT %1%==--no-update (
+         if NOT %1%==-nu (
+            if exist .\transgui\ (
+               :: Remove old repository files
+               rm -rf .\transgui
+               )
+            :: Clone the repository
+            git clone https://github.com/transmission-remote-gui/transgui.git
+            )
+         )
+      )
+   )
 
 
 cd .\transgui\setup\win_amd64
