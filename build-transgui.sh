c974a96395d33cade693a559e699b9b8a3d1b8d8
Max-Gouliaev <68902211+Max-Gouliaev@users.noreply.github.com>
2023-05-25 00:09:37 +0300

    (#1) Now installs required packages before actually using them

--- build-transgui.sh
+++ build-transgui.sh
@@ -3,6 +3,35 @@ now=$(date +%F)
 
 # Colors
 LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+
+
+# Check if the packages are installed
+if [ ! -x "$(command -v git)" ] || [ ! -x "$(command -v make)" ] || [ ! -x "$(command -v tar)" ] || [ ! -x "$(command -v lazbuild)" ] || [ ! -x "$(command -v lazarus)" ] || [ ! -x "$(command -v gh)" ]; then
+  printf "%s""$LRED""Packages are not installed!\n"
+  
+  # Detect package manager
+  file=/etc/os-release
+    # Debian
+    if grep -q 'ID_LIKE=.*debian.*' "$file" || grep -q 'ID_LIKE=.*ubuntu.*' "$file"; then
+      printf "%s""$LGREEN""Detected Debian based system.\n"
+      apt-get -y install git make tar lazarus github-cli
+    # RPM-based
+    elif grep -q 'ID_LIKE=.*centos.*' "$file" || grep -q 'ID_LIKE=.*fedora.*' "$file"; then
+      printf "%s""$LGREEN""Detected rpm based system.\n"
+      yum -y install git make tar lazarus github-cli
+    # Arch
+    elif grep -q 'ID_LIKE=.*arch.*' "$file"; then
+      printf "%s""$LGREEN""Detected Arch based system.\n"
+      sudo pacman -S --needed git make tar lazarus github-cli
+    # Unknown
+    else
+      printf "%s""$LRED""Could not detect the system type.\n" "%s" "$LGREEN" "Please install these packages: git, make, tar, lazarus, github-cli\n"
+      # pause until keypress
+      read -n 1 -s -r -p "Press any key when you have installed these packages..."
+    fi
+fi
+
 
 # Check the arguments
 if [ ! "$1" = --no-clone ] && [ ! "$1" = -nc ] && [ ! "$1" = --no-update ] && [ ! "$1" = -nu ]; then
